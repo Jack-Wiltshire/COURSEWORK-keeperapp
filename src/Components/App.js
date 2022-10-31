82fd39773ca38ed43538fab8705060a37058139f
Jack Wiltshire <jackwiltshire.webdev@gmail.com>
2022-11-01 10:22:19 +1100

    Finished product

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import CreateArea from "./CreateArea";
 
 function App() {
+
+  const [notes, setNotes] = useState([]);
+
+  function addNote(note) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, note];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((note, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return <div>
     <Header />
-    <Note />
+    <CreateArea onAdd={addNote} />
+    {notes.map((note, index) => (
+      <Note
+        key={index}
+        id={index}
+        title={note.title}
+        content={note.title}
+        onDelete={deleteNote} />
+    ))}
     <Footer />
   </div>
 };
--- src/Components/Note.js
+++ src/Components/Note.js
@@ -1,9 +1,14 @@
 import React from "react";
+import DeleteIcon from '@mui/icons-material/Delete';
 
-function Note() {
+function Note(props) {
+  function handleClick(){
+    props.onDelete(props.id)
+  }
   return <div className="note">
-    <h1>Note Title</h1>
-    <p>Note Content</p>
+    <h1>{props.title}</h1>
+    <p>{props.content}</p>
+    <button onClick={handleClick}><DeleteIcon /></button>
   </div>
 };
 
